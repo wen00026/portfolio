225f19f7b0639eca8f583bd06797501c6ae47be9
Fang Wen <wen00026@algonquinlive.com>
2020-01-13 16:02:49 -0500

    add slider.js file

---
+++ slider.js
@@ -0,0 +1,25 @@
+var slideIndex = 1
+displaySlides(slideIndex)
+
+function nextSlide (n) {
+  displaySlides(slideIndex += n)
+}
+
+function currentSlide (n) {
+  displaySlides(slideIndex = n)
+}
+
+function displaySlides (n) {
+  var i
+  var slides = document.getElementsByClassName('showSlide')
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  slides[slideIndex - 1].style.display = 'block'
+}
